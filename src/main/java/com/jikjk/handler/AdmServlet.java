976e8565d0c231fff9a7752a489384d9fc89b4cb
lenovo <2549501697@qq.com>
2018-10-14 23:01:05 +0800

    1.6游客发送简历，面试，管理查看录取（无页面）

---
+++ src/main/java/com/jikjk/handler/AdmServlet.java
@@ -0,0 +1,165 @@
+package com.jikjk.handler;
+
+import com.jikjk.entity.*;
+import com.jikjk.entity.respojo.ResMassageResume;
+import com.jikjk.service.*;
+import com.jikjk.service.impl.InterviewResultServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * Created by lenovo on 2018/10/14.
+ */
+@RequestMapping("adm")
+@Controller
+public class AdmServlet {
+    @Autowired
+    private InviteJobService inviteJobServiceImpl;
+    @Autowired
+    private MassageResumeService massageResumeServiceImpl;
+    @Autowired
+    private SendResumeService sendResumeServiceImpl;
+    @Autowired
+    private ResumeService resumeServiceImpl;
+    @Autowired
+    private InterviewService interviewServiceImpl;
+    @Autowired
+    private InterviewResultService interviewResultServiceImpl;
+
+    @RequestMapping("sendInvite")
+    public String sendInviteJob(){
+        return "admSendInvite";
+    }
+    @RequestMapping("commitSendInvite")
+    public String commitSendInvite(InviteJob inviteJob){
+        inviteJobServiceImpl.insert(inviteJob);
+        return "admPage";
+    }
+
+    /**
+     * 查看所有简历
+     * @param map
+     * @return
+     */
+    @RequestMapping("lookResume")
+    public String lookResume(ModelMap map){
+        List<ResMassageResume> massageResumes=massageResumeServiceImpl.selectAll();
+        map.addAttribute("massageResumes",massageResumes);
+        return "admLookResume";
+    }
+    /**
+     * 查看个人简历
+     * @param request
+     * @return
+     */
+    @RequestMapping("lookingResume")
+    public String lookingResume(HttpServletRequest request,ModelMap map){
+        int rId=0;
+        try {
+            rId=Integer.valueOf(request.getParameter("rId"));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        //修改sendResmue的查看状态
+        sendResumeServiceImpl.updateReadState(rId,"已查看");
+        //修改简历记录查看状态
+        massageResumeServiceImpl.updateStateRead(rId,"已查看");
+        Resume resume=resumeServiceImpl.selectByRid(rId);
+        map.addAttribute("resume",resume);
+        return "admLookingResume";
+    }
+
+    /**
+     * 删除简历
+     * @param request
+     * @return
+     */
+    @RequestMapping("deleteMasResume")
+    public String deleteMasResume(HttpServletRequest request){
+        int rId=0;
+        try {
+            rId=Integer.valueOf(request.getParameter("rId"));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        massageResumeServiceImpl.delete(rId);
+        return "admLookResume";
+    }
+
+    /**
+     * 创建面试
+     * @return
+     */
+    @RequestMapping("interview")
+    public String interview(HttpServletRequest request,ModelMap map){
+        int rId=0;
+        try {
+            rId=Integer.valueOf(request.getParameter("rId"));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        Resume resume=resumeServiceImpl.selectByRid(rId);
+        map.addAttribute("resume",resume);
+        return "admInterview";
+    }
+
+    /**
+     * 面试通知
+     * @param interview
+     * @return
+     */
+    @RequestMapping("interviewInform")
+    public String interviewInform(Interview interview){
+        interviewServiceImpl.insert(interview);
+        return "admLookingResume";
+    }
+
+    /**
+     * 返回查看简历
+     * @return
+     */
+    @RequestMapping("gotoLookResume")
+    public String gotoLookResume(){
+        return "forward:lookResume";
+    }
+
+    /**
+     * 面试结果
+     * @param request
+     * @param map
+     * @return
+     */
+    @RequestMapping("intvResult")
+    public String intvResult(HttpServletRequest request,ModelMap map){
+        int rId=0;
+        try {
+            rId=Integer.valueOf(request.getParameter("rId"));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        InterviewResult interviewResult=interviewResultServiceImpl.selectByRid(rId);
+        Interview interview=interviewServiceImpl.selectByRid(rId);
+        Resume resume=resumeServiceImpl.selectByRid(rId);
+        map.addAttribute("resume",resume);
+        map.addAttribute("interview",interview);
+        map.addAttribute("interviewResult",interviewResult);
+        return "admIntvResult";
+    }
+
+    /**
+     * 添加面试结果
+     * @param interviewResult
+     * @return
+     */
+    @RequestMapping("addIntvResult")
+    public String addIntvResult(InterviewResult interviewResult){
+        interviewResultServiceImpl.insert(interviewResult);
+        sendResumeServiceImpl.updateInformState(interviewResult.getrId(),"有通知");
+        return "forward:interviewInform";
+    }
+}
